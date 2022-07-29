b1598dfd9f56b1df91fc69a9579440e0e7a25c6a
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-29 11:42:20 -0400

    assigned global variables for current weather and city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 const key = '4a94948c09b423a41e87f9c16b95831e'
 const searchBtn = document.getElementById('search-btn')
 var userInput = document.getElementById('city-search')
+var currentCityWeather = {}
+var currentCity = ''
+
 
 async function geoLocate(e) {
     e.preventDefault();
@@ -15,10 +18,17 @@ async function geoLocate(e) {
 async function todaysForecast(geoData){
 var lat = geoData[0].lat
 var lng = geoData[0].lon
+
+   var weather = await fetch ('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lng + '&units=imperial&appid=' + key)
+    var formattedWeather = await weather.json()
+    currentCityWeather = formattedWeather.current
+    currentCity = formattedWeather.name
+    
 }
 
 async function fiveDay(geoData){
-console.log(geoData)
+    var lat = geoData[0].lat
+    var lng = geoData[0].lon
 }
 
 searchBtn.addEventListener('click', geoLocate)
