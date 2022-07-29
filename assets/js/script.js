89ed95e1e03a10108df66575fe78abee8e3253a6
Ethan Cahill <andersmith29@gmail.comhtml lang=en  head    meta charset=UTF-8 /  /head  body    p      I LOVE PANDASgit config --global user.email andersmith29@gmail.com>
2022-07-29 11:28:53 -0400

    grabbed lng and lat

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 const key = '4a94948c09b423a41e87f9c16b95831e'
-const searchBtn = document.getElementById('search')
+const searchBtn = document.getElementById('search-btn')
 var userInput = document.getElementById('city-search')
 
 async function geoLocate(e) {
@@ -7,7 +7,18 @@ async function geoLocate(e) {
 
     const locationResponse= await fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + userInput.value + '&limit=1&appid=' + key)
    const formattedResponse = await locationResponse.json()
-   console.log(formattedResponse)
+   
+   todaysForecast(formattedResponse)
+   fiveDay(formattedResponse)
+}
+
+async function todaysForecast(geoData){
+var lat = geoData[0].lat
+var lng = geoData[0].lon
+}
+
+async function fiveDay(geoData){
+console.log(geoData)
 }
 
 searchBtn.addEventListener('click', geoLocate)
